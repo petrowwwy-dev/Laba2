a7f54797d0a1f15b63b77776496e667a541ed946
Petro Kolotylo <petrowwwy@gmail.com>
2026-03-20 00:15:22 +0200

    Changing class Product

--- laba2/Product.cpp
+++ laba2/Product.cpp
@@ -1,11 +1,14 @@
 #include "Product.h" 
 #include <iostream>
+#include <utility>
 
 using namespace std;
+int Product::totalProducts = 0;
 
 
 Product::Product(int pId, string pName, double pPrice, int pStock)
     : id(pId), name(pName), price(pPrice), stock(pStock) {
+    totalProducts++;
     cout << "Створено товар: " << name << endl;
 }
 
@@ -15,12 +18,32 @@ Product::Product(int pId, string pName) : Product(pId, pName, 0.0) {
 }
 
 
+Product::Product(const Product& other)
+    : id(other.id), name(other.name), price(other.price), stock(other.stock) {
+    totalProducts++;
+    cout << "[Копіювання] Створено точну копію товару: " << name << endl;
+}
+
+
+Product::Product(Product&& other) noexcept
+    : id(other.id), name(std::move(other.name)), price(other.price), stock(other.stock) {
+
+    other.id = 0;
+    other.price = 0.0;
+    other.stock = 0;
+
+    totalProducts++; 
+    cout << "[Переміщення] Дані товару '" << name << "' успішно переміщено!" << endl;
+}
+
+
 Product::~Product() {
+    totalProducts--;
     cout << "Товар '" << name << "' видалено з пам'яті." << endl;
 }
 
 
-void Product::printInfo() {
+void Product::printInfo() const {
     cout << "--- Товар #" << id << " ---" << endl;
     cout << "Назва: " << name << endl;
     cout << "Ціна: " << price << " грн" << endl;
@@ -34,4 +57,39 @@ void Product::updateStock(int amount) {
 
     stock += amount;
 
+}
+
+
+int Product::getTotalProducts() {
+    return totalProducts;
+}
+
+Product& Product::operator++() {
+    this->stock++;
+    return *this;
+}
+
+    Product Product::operator+(int extraStock) const {
+    Product temp = *this; 
+    temp.stock += extraStock; 
+    return temp; 
+}
+
+
+ostream& operator<<(ostream& os, const Product& p) {
+    
+    os << "[" << p.name << "] ID: " << p.id
+        << " | Ціна: " << p.price << " грн | На складі: " << p.stock << " шт.";
+    return os;
+}
+
+istream& operator>>(istream& is, Product& p) {
+    cout << "Введіть ID товару: ";
+    is >> p.id;
+    cout << "Введіть назву товару: ";
+    is.ignore(); 
+    getline(is, p.name);
+    cout << "Введіть ціну та кількість на складі (через пробіл): ";
+    is >> p.price >> p.stock;
+    return is;
 }
--- laba2/Product.h
+++ laba2/Product.h
@@ -8,6 +8,8 @@ private:
     double price;
     int stock;
 
+    static int totalProducts;
+
 public:
 
 
@@ -15,8 +17,20 @@ public:
 
     Product(int pId, std::string pName);
 
+    Product(const Product& other);
+
+    Product(Product&& other) noexcept;
+
     ~Product();
 
-    void printInfo();
+    void printInfo() const;
     void updateStock(int amount);
+
+    static int getTotalProducts();
+
+    Product& operator++();
+    Product operator+(int extraStock) const;
+    friend std::ostream& operator<<(std::ostream& os, const Product& p);
+    friend std::istream& operator>>(std::istream& is, Product& p);
+
 };
