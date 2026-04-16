b51790bcea4d58254ff0ab4d2be4eddb21c3a570
Petro Kolotylo <petrowwwy@gmail.com>
2026-04-16 19:24:18 +0300

    Change laba2.cpp

--- laba2/Laba2.cpp
+++ laba2/Laba2.cpp
@@ -5,6 +5,8 @@
 #include "Customer.h"
 #include "Order.h"
 #include "Electronics.h"
+#include "Smartphone.h"
+#include "Laptop.h"
 
 using namespace std;
 
@@ -38,17 +40,7 @@ int main() {
     cout << "\n=== РОБОТА З CONST ОБ'ЄКТОМ ===" << endl;
     const Product p_const(99, "Архівний товар", 0.0, 0);
     p_const.printInfo(); 
-
-    cout << "\n=== ВВЕДЕННЯ НОВОГО ТОВАРУ ===" << endl;
-    Product p_custom(100, "Пустишка");
-    cout << "-> Будь ласка, введіть дані для нового товару:" << endl;
-    cin >> p_custom; 
-    cout << "\nВи успішно додали: " << p_custom << endl;
-
-
-    
-
-    
+ 
     Customer c1(1, "Олександр", "sasha@gmail.com");
     Customer c2(2, "Марія");
 
@@ -59,22 +51,72 @@ int main() {
 
     cout << "\n=== ТЕСТУВАННЯ КОМПОЗИЦІЇ ТА НАСЛІДУВАННЯ ===" << endl;
 
-    // Створюємо звичайне замовлення з товаром p1, який у тебе вже є вище
+   
     Order order1(101, p1, 1500.50);
     order1.printOrder();
     order1.pay();
 
 
-    // А тепер створимо наш новий об'єкт Electronics (Електроніка)
+   
     Electronics phone(200, "iPhone 15", 45000.0, 10, 12, "Apple");
 
-    // І покладемо цю електроніку в друге замовлення!
+
     Order order2(102, phone, 45000.0);
     order2.printOrder();
 
     order2.pay();
 
+    Smartphone s1(301, "Galaxy S24", 38000.0, 15, 24, "Samsung", "Android", 6.2);
+    s1.printInfo();
 
+ 
+    cout << "\n--- Copy конструктор Smartphone ---" << endl;
+    Smartphone s2 = s1;
+    s2.printInfo();
+
+    cout << "\n--- Move конструктор Smartphone ---" << endl;
+    Smartphone s3 = std::move(s2);
+    s3.printInfo();
+
+    cout << "\n--- Operator= Smartphone ---" << endl;
+    Smartphone s4(302, "Pixel 8", 32000.0, 5, 12, "Google", "Android", 6.1);
+    s4 = s1;
+    s4.printInfo();
+
+    cout << "\n--- Замовлення смартфону ---" << endl;
+    Order order3(103, s1, 38000.0);
+    order3.printOrder();
+    order3.pay();
+
+    cout << "\n=== ТЕСТУВАННЯ LAPTOP (3-й рівень ієрархії) ===" << endl;
+
+    Laptop l1(401, "MacBook Pro", 95000.0, 3, 12, "Apple", 16, "M3 Pro");
+    l1.printInfo();
+
+    cout << "\n--- Copy конструктор Laptop ---" << endl;
+    Laptop l2 = l1;
+    l2.printInfo();
+
+    cout << "\n--- Move конструктор Laptop ---" << endl;
+    Laptop l3 = std::move(l2);
+    l3.printInfo();
+
+    cout << "\n--- Operator= Laptop ---" << endl;
+    Laptop l4(402, "ThinkPad X1", 55000.0, 7, 24, "Lenovo", 32, "Intel i7");
+    l4 = l1;
+    l4.printInfo();
+
+    cout << "\n--- Замовлення ноутбука ---" << endl;
+    Order order4(104, l1, 95000.0);
+    order4.printOrder();
+    order4.pay();
+
+
+    cout << "\n=== ВВЕДЕННЯ НОВОГО ТОВАРУ ===" << endl;
+    Product p_custom(100, "Пустишка");
+    cout << "-> Будь ласка, введіть дані для нового товару:" << endl;
+    cin >> p_custom;
+    cout << "\nВи успішно додали: " << p_custom << endl;
 
 
 
