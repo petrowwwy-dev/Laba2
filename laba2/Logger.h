b04046e94deb7c17d711b95d31e75858d354918b
Petro Kolotylo <petrowwwy@gmail.com>
2026-05-12 21:32:16 +0300

    add Logger

---
+++ laba2/Logger.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+class Logger {
+public:
+    static void log(const std::string& action) {
+        std::ofstream file("history.txt", std::ios::app); 
+        if (file.is_open()) {
+            
+            auto now = std::chrono::system_clock::now();
+            std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+            char timeBuf[26];
+            ctime_s(timeBuf, sizeof(timeBuf), &now_c);
+            std::string timeStr(timeBuf);
+            timeStr.pop_back(); 
+
+            file << "[" << timeStr << "] " << action << "\n";
+            file.close();
+        }
+    }
+};
